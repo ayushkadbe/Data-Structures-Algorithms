f7f2357ecbdc07721c9c836f7e9414fdce66d440
Ayush Kadbe <67881141+ayushkadbe@users.noreply.github.com>
2022-07-02 11:43:00 +0530

    Pointer Array Traversing, Pointer to Array, Pointer to Array of Function

--- PointerToArray.c
+++ PointerToArray.c
@@ -16,6 +16,7 @@ int main(){
 
 //PROGRAM 2: POINTER TO FUNCTION IN C
 
+
 /*
 Create pointer variable with function declaration
 assign address of function to pointer
@@ -40,3 +41,42 @@ int multiplication(){
 }
 
 //PROGRAM 3: POINTER TO ARRAY OF FUNCTION IN C
+
+/*Basically, an array of the function is an array which contains the addresses of functions. In other words, the pointer 
+to an array of functions is a pointer pointing to an array which contains the pointers to the functions. Consider the following example.*/
+
+/*ALGORITHM:
+1. declare an array function
+2. declare an pointer to array function.
+2. create elements of array: Pointers/Address to functions
+2. initialize pointer to array of functions(address/pointer to function)
+3. create a object to call pointer to element 1 of array of function i.e (double pointer)
+4. Call pointer to array of function i.e (double pointer)
+5. Call pointer + 1 to call Element 2 of array of function(passing value of pointer to array)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int show();
+int showadd(int);
+int (*arr[3])();
+int (*(*ptr)[3])();
+
+int main(){
+    arr[0] = show;
+    arr[1] = showadd;
+    ptr = &arr;
+    int result = (**ptr)();
+    printf("\n Print Pointer to Array Value:%d", result);
+    (*(*ptr+1))(result);
+
+}
+
+int show(){
+   int a = 60;
+   return a++;
+}
+
+int showadd(int b){
+    printf("\n Add 50 to number passed by result: %d", b+ 50);
+    
+}
